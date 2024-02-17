f5d17de3c2b8b6b9abab3a373445e4ccdb8e3cf8
Gaetan LAVIT <g.lavit@outlook.fr>
2024-02-17 18:31:47 +0400

    update animation css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-2024';
+  ngOnInit() {
+    let section = document.getElementById('tech-container')
+    window.onscroll = () => {
+      if (section) {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 600
+        let height = section.offsetHeight
+        let cards = Array.from(document.getElementsByClassName('tech-card'))
+        cards.forEach(element => {
+          element.classList.add('tech-card-showed')
+        });
+          if (top >= offset && top < offset + height) {
+            section?.classList.add('show-animate')
+          }
+      }
+    }
+  }
 }
