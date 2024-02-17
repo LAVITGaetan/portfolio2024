83af340a14ac301cdcf07792f3f0334807b13997
Gaetan LAVIT <g.lavit@outlook.fr>
2024-02-17 23:13:27 +0400

    ajout de la modal demo avec recuperation d'iframe youtube

--- src/app/views/project/project.component.ts
+++ src/app/views/project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-project',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent {
- 
+  constructor(private sanitize: DomSanitizer) { }
+  demoIsShowed : boolean = false
+  youtube_url = "https://www.youtube.com/embed/"
+  video_url = ""
+  setUrl() {
+    return this.sanitize.bypassSecurityTrustResourceUrl(`${this.youtube_url}${this.video_url}`)
+  }
+
+  showDemo(url: string) {
+    this.demoIsShowed = true
+    this.video_url = url
+    this.setUrl()
+  }
+
+  
+  closeDemo() {
+    this.demoIsShowed = false
+    this.video_url = ""
+  }
 }
