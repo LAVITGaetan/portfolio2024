bdd2ccf23a649498c99aa740ac41bda04d90e23b
Gaetan LAVIT <g.lavit@outlook.fr>
2024-02-17 14:28:17 +0400

    ajout d' animations css basique lors du scroll et du chargement de la page

--- src/app/views/project/project.component.ts
+++ src/app/views/project/project.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent {
-
+ 
 }
--- src/app/views/tech/tech.component.ts
+++ src/app/views/tech/tech.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./tech.component.css']
 })
 export class TechComponent {
+  ngOnInit() {
+    let section = document.getElementById('tech-container')
+    window.onscroll = () => {
+      if (section) {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 600
+        let height = section.offsetHeight
+        let cards = Array.from(document.getElementsByClassName('tech-card'))
+        cards.forEach(element => {
+          element.classList.add('tech-card-showed')
+        });
+          if (top >= offset && top < offset + height) {
+            section?.classList.add('show-animate')
+          }
+      }
+    }
+  }
 
 }
